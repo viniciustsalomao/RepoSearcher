4120a7f3e2ca8fb5338093c7329bb22e8cd302a0
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-10-26 20:56:36 -0300

    Initial Commit

---
+++ GitHubRepoSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubRepoSearcher
+//
+//  Created by Vinícius Tinajero Salomão on 26/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
